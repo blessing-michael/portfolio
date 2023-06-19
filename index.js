0850a4fe17d1d0c3beb79038a1912f7d1f14c71f
Hsu Waddy <hsuwady2001@gmail.com>
2023-06-19 14:42:09 +0630

    Add-eventlistener-to-mobilemenu

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ const Navicon = document.querySelector('.bar-div');
 const links = document.querySelector('.nav-info');
 const linksContainer = document.querySelector('.links-container');
 const linksblur = document.querySelector('.blur');
-
-
+Navicon.addEventListener('click', () => {
+    console.log('clicked');
+    links.classList.toggle('active')
+        // linksContainer.classList.add('.blurry')
+});
