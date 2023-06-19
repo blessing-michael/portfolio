3f58aca38bdf592c718612f8ceeb4c7292f07d11
blessing-michael <blessingmike334@gmail.com>
2023-06-19 10:02:58 -0700

    Add javascript selectors to index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const Navicon = document.querySelector(".bar-div");
+const links = document.querySelector(".nav-info");
+const linksContainer = document.querySelector(".links-container");
+const linksblur = document.querySelector(".blur");
+
+
